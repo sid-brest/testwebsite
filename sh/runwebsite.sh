c2c314bfaeeabdc77be9c57584d1bc98a17180fc
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-10-24 21:46:11 +0300

    update to any github url

--- sh/runwebsite.sh
+++ sh/runwebsite.sh
@@ -1,72 +1,69 @@
 #!/bin/bash
 
+set -e
+
+# Check if the URL parameter is provided
+if [ -z "$1" ]; then
+    echo "Error: URL parameter is missing."
+    echo "Please provide the URL to clone the repository as a parameter."
+    echo "Example: ./script.sh https://github.com/username/reponame.git"
+    exit 1
+fi
+
 # Define the Git clone link as a variable
-repo_url="https://github.com/sid-brest/testwebsite.git"
+repo_url="$1"
 
-# Check if Nginx is installed
-if ! command -v nginx &> /dev/null; then
-    echo "Nginx is not installed. Installing Nginx..."
-    sudo apt update
-    sudo apt install -y nginx
-else
-    echo "Nginx is already installed."
-fi
+# Create a temporary directory inside the home directory
+temp_dir="$HOME/temp_website"
+echo "Creating temporary directory: $temp_dir"
+mkdir -p "$temp_dir"
 
-# Check if Git is installed
-if ! command -v git &> /dev/null; then
-    echo "Git is not installed. Installing Git..."
-    sudo apt install -y git
-else
-    echo "Git is already installed."
-fi
+# Install required packages
+packages=("nginx" "git" "ufw")
+for package in "${packages[@]}"; do
+    if ! command -v "$package" &> /dev/null; then
+        echo "$package is not installed. Installing $package..."
+        sudo apt update
+        sudo apt install -y "$package"
+    else
+        echo "$package is already installed."
+    fi
+done
 
-# Check if Nginx is enabled
-if sudo systemctl is-enabled nginx &> /dev/null; then
-    echo "Nginx is already enabled."
-else
+# Enable Nginx if not already enabled
+if ! sudo systemctl is-enabled nginx &> /dev/null; then
     echo "Enabling Nginx..."
     sudo systemctl enable nginx
 fi
 
-# Remove existing testwebsite directory
-if [ -d "testwebsite" ]; then
-    echo "Removing existing testwebsite directory..."
-    sudo rm -rf testwebsite
-fi
+# Remove existing files in the default Nginx directory
+echo "Clearing /var/www/html/..."
+sudo rm -rf /var/www/html/*
 
-# Clone the repository
-echo "Cloning the repository..."
-git clone $repo_url
+# Clone the repository into the temporary directory
+echo "Cloning the repository into $temp_dir..."
+git clone "$repo_url" "$temp_dir"
 
 # Move the files to the default Nginx directory
 echo "Copying files to /var/www/html/..."
-sudo rm -rf /var/www/html/*
-sudo cp -r -v testwebsite/* /var/www/html/
-
-# Check if ufw is installed
-if ! dpkg -s ufw &> /dev/null; then
-    echo "ufw is not installed. Installing..."
-    sudo apt install -y ufw
-else
-    echo "ufw is already installed."
-fi
+sudo cp -r -v "$temp_dir"/* /var/www/html/
 
 # Configure firewall rules for SSH (port 22) and Nginx (port 80) using ufw
 echo "Configuring firewall rules using ufw..."
-sudo ufw allow 22/tcp     # Allow SSH traffic
-sudo ufw allow 80/tcp     # Allow Nginx (HTTP) traffic
-sudo ufw --force enable   # Enable the firewall
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw --force enable
 
 # Start Nginx service
 echo "Starting Nginx..."
 sudo systemctl start nginx
 
 # Retrieve IPv4 addresses
-ipv4_addresses=$(hostname -I | tr ' ' '\n')
+ipv4_addresses=($(hostname -I | tr ' ' '\n'))
 
 # Filter local LAN addresses starting with "192"
 matching_addresses=()
-for address in $ipv4_addresses; do
+for address in "${ipv4_addresses[@]}"; do
     if [[ $address == 192.* ]]; then
         matching_addresses+=("$address")
     fi
@@ -83,7 +80,7 @@ else
 fi
 
 # Retrieve the Public IPv4 address using AWS CLI
-public_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+public_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4 || true)
 
 if [ -n "$public_ip" ]; then
     # Public IPv4 address is available
@@ -91,3 +88,7 @@ if [ -n "$public_ip" ]; then
 else
     echo "Failed to retrieve the Public IPv4 address."
 fi
+
+# Clean up the temporary directory
+echo "Cleaning up the temporary directory: $temp_dir"
+rm -rf "$temp_dir"
